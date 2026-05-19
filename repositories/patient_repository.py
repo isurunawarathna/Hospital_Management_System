f3a3afd3110eb20a2646fe5f5fa3b5a9616c1071
IsuruNawarathna <isurunawarathna2006@gmail.com>
2026-05-19 14:29:39 +0530

    Complete the Patient Repository

--- repositories/patient_repository.py
+++ repositories/patient_repository.py
@@ -1,6 +1,37 @@
-from dataclasses import dataclass
+import os.path
+from models.Patient import Patient
 
 class PatientRepository:
 
     def __init__(self):
-        base_dir = {}
+        ## Project folder Path
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.file_path = os.path.join(base_dir,"data","patient.txt")
+        self.save_all()
+
+    def find_all(self):
+        patients = []
+
+        if not os.path.exists(self.file_path):
+            return patients
+        with open(self.file_path,"r") as file:
+            for line in file:
+                if line.strip():
+                    patients.append(Patient.from_text(line))
+        return patients
+
+    def add(self,patient):
+
+        patients = self.find_all()
+        patients.append(patient)
+
+    def save_all(self,patients):
+        with open(self.file_path,"w") as file:
+            for patient in patients:
+                file.write(patient.to_text()+"\n")
+
+
+
+
+
+
